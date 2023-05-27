c84479cd0c03016549dbc9e31d10baab9b1ec84c
Joey Marino <joey.da3rd@gmail.com>
2023-05-27 04:43:12 +0000

    added setup.py to repo

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='pyHM310T',
+    version='0.1.0',
+    description='Python library for controlling the Hanmatek HM310T power supply',
+    author='Joey Marino',
+    author_email='joey.da3rd@gmail.com',
+    url='https://github.com/joeyda3rd/modbus-power-supply',
+    packages=['pyHM310T'],
+    install_requires=[
+        'pymodbus',
+        'pyserial',
+    ],
+)
+
